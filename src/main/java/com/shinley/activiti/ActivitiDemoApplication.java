4e1350b3e0f733518bf9885112b1f4fed9969009
shinley <cxl17258@qq.com>
2019-08-06 20:48:58 +0800

    能正常启动

--- src/main/java/com/shinley/activiti/ActivitiDemoApplication.java
+++ src/main/java/com/shinley/activiti/ActivitiDemoApplication.java
@@ -10,9 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActivitiDemoApplication {
 
-	@Autowired
-	private TaskService taskService;
-
 
 
 	public static void main(String[] args) {
