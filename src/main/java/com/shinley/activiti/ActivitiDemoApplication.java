647ca2e187b60c2a680c50f4990fffa0803d389f
shinley <chenxl4@autoai.com>
2019-08-06 15:17:03 +0800

    配置activiti

--- src/main/java/com/shinley/activiti/ActivitiDemoApplication.java
+++ src/main/java/com/shinley/activiti/ActivitiDemoApplication.java
@@ -1,11 +1,12 @@
 package com.shinley.activiti;
 
 import org.activiti.engine.TaskService;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@MapperScan("com.shinley.activiti.dao")
 @SpringBootApplication
 public class ActivitiDemoApplication {
 
---
+++ src/main/java/com/shinley/activiti/config/ActivitiConfig.java
@@ -0,0 +1,122 @@
+package com.shinley.activiti.config;
+
+import org.activiti.engine.*;
+import org.activiti.engine.delegate.event.ActivitiEventListener;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivitiConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private EventListener eventListener;
+
+    /**
+     * 初始化配置，创建流程引擎表
+     *
+     * @return
+     */
+    @Bean
+    public StandaloneProcessEngineConfiguration processEngineConfiguration() {
+        StandaloneProcessEngineConfiguration configuration = new StandaloneProcessEngineConfiguration();
+        configuration.setDataSource(dataSource);
+        // 设置是否自动更新
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        configuration.setAsyncExecutorActivate(false);
+
+        //configuration.setCustomMybatisMappers(Collections.singleton(ProcessMapper.class));
+        //configuration.setActiviti5CustomMybatisXMLMappers(Collections.singleton("classpath:mapper/ProcessMapper.xml"));
+        // 注册全局监听器
+        List<ActivitiEventListener> activitiEventListener=new ArrayList<>();
+        activitiEventListener.add(eventListener);
+        configuration.setEventListeners(activitiEventListener);
+        return configuration;
+    }
+
+    /**
+     * 初始化配置，创建流程引擎表
+     *
+     * @return
+     */
+    @Bean
+    public ProcessEngine getProcessEngine() {
+        return processEngineConfiguration().buildProcessEngine();
+    }
+
+    /**
+     * 仓库服务
+     *
+     * @return
+     */
+    @Bean
+    public RepositoryService getRepositoryService() {
+        return getProcessEngine().getRepositoryService();
+    }
+
+    /**
+     * 运行时服务
+     *
+     * @return
+     */
+    @Bean
+    public RuntimeService getRuntimeService() {
+        return getProcessEngine().getRuntimeService();
+    }
+
+    /**
+     * 表单服务
+     *
+     * @return
+     */
+    @Bean
+    public FormService getFormService() {
+        return getProcessEngine().getFormService();
+    }
+
+    /**
+     * 任务服务
+     *
+     * @return
+     */
+    @Bean
+    public TaskService getTaskService() {
+        return getProcessEngine().getTaskService();
+    }
+
+    /**
+     * 历史服务
+     *
+     * @return
+     */
+    @Bean
+    public HistoryService getHistoryService() {
+        return getProcessEngine().getHistoryService();
+    }
+
+    /**
+     * 认证服务
+     *
+     * @return
+     */
+    @Bean
+    public IdentityService getIdentityService() {
+        return getProcessEngine().getIdentityService();
+    }
+
+    /**
+     * 管理服务
+     *
+     * @return
+     */
+    @Bean
+    public ManagementService getManagementService() {
+        return getProcessEngine().getManagementService();
+    }
+}
