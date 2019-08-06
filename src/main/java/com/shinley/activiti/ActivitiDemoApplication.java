1ce2a55e3f596315333c36cc80b0e1d872da214d
shinley <chenxl4@autoai.com>
2019-08-06 10:55:50 +0800

    配置数据源

--- src/main/java/com/shinley/activiti/ActivitiDemoApplication.java
+++ src/main/java/com/shinley/activiti/ActivitiDemoApplication.java
@@ -1,11 +1,18 @@
 package com.shinley.activiti;
 
+import org.activiti.engine.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+
 
 @SpringBootApplication
 public class ActivitiDemoApplication {
+
+	@Autowired
+	private TaskService taskService;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivitiDemoApplication.class, args);
